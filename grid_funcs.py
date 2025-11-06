50d0763eb128e025dadeda424069a87f8a181e82
astralrain <rinasong0617@gmail.com>
2025-11-06 15:21:52 -0500

    edited grid functions, added add_good_fraction

--- grid_funcs.py
+++ grid_funcs.py
@@ -2,23 +2,20 @@ from libs import *
 from consts import *
 from basic_funcs import *
 
-def grid_tile(cat_name: str, cat_df: pd.DataFrame, ngrid: int = 10,
-              padding_fraction: float = 0.0) -> pd.DataFrame:
+def grid_tile(tile: str, cat_df: pd.DataFrame, 
+              ngrid: int = 10) -> pd.DataFrame:
     """
     Grids a CFIS tile using pixel coordinates.
 
     Parameters:
-        cat_name (str): Name of the tile/catalogue of interest.
-        cat_df (pd.DataFrame): DataFrame containing the catalogue file of the tile.
+        tile (str): Name of the tile/catalogue of interest.
+        cat_df (pd.DataFrame): DataFrame containing the catalogue 
+            file of the tile.
         ngrid (int): Number of grid divisions in X and Y, default = 10.
-        padding_fraction (float): Padding beyond min/max extents, as a fraction, default = 0.0.
 
     Returns:
         Output CSV path of gridded catalogue
     """
-
-    x_min, x_max = 0.0, TILE_SIZE
-    y_min, y_max = 0.0, TILE_SIZE
 
     x_step = TILE_SIZE / ngrid
     y_step = TILE_SIZE / ngrid
@@ -35,28 +32,27 @@ def grid_tile(cat_name: str, cat_df: pd.DataFrame, ngrid: int = 10,
     cat_df["i"] = x_idx
     cat_df["j"] = y_idx
 
-    output_file = os.path.join(GRID_DIR, f"{cat_name}_gridded.csv")
+    output_file = os.path.join(GRID_DIR, f"{tile}_gridded.csv")
     cat_df.to_csv(output_file, index=False)
     
     print(f"Pixel-based gridded catalogue written to {output_file}")
 
     return output_file
 
-def compute_grid_centers(cat_name: str, cat_df: pd.DataFrame, ngrid: int = 10):
+def compute_grid_centers(tile: str, ngrid: int = 10):
     """
     Compute RA/Dec centers of each grid using pixel coordinates and WCS.
     Grids are defined in pixel space, then converted to sky coordinates.
 
     Parameters:
-        cat_name (str): Name of the tile/catalogue of interest.
-        cat_df (pd.DataFrame): DataFrame containing the catalogue file of the tile.
+        tile (str): Name of the tile/catalogue of interest.
         ngrid (int): Number of grid divisions in X and Y, default = 10.
     
     Returns:
         Output CSV path of the grid centers file.
     """
 
-    wcs, nx, ny = load_header(cat_name)
+    wcs, nx, ny = load_header(tile)
     
     print(f"Loaded WCS. Image size = {nx} x {ny} pixels")
 
@@ -89,19 +85,20 @@ def compute_grid_centers(cat_name: str, cat_df: pd.DataFrame, ngrid: int = 10):
             })
 
     grid_info = pd.DataFrame(grid_data)
-    output_file = os.path.join(GRID_CENTER_DIR, cat_name + "_grid_centers.csv")
+    output_file = os.path.join(GRID_CENTER_DIR, tile + "_grid_centers.csv")
     grid_info.to_csv(output_file, index=False)
 
     print(f"Grid centers written to {output_file}")
 
     return output_file
 
-def flag_regions(cat_name: str, gridded_file: str) -> pd.DataFrame:
+def flag_regions(tile: str, gridded_file: str) -> pd.DataFrame:
     """
-    Given a catalogue and its tile, mark which objects fall in good or bad regions using masks.
+    Given a catalogue and its tile, mark which objects fall in good or 
+    bad regions using masks.
 
     Parameters:
-        cat_name (str): Name of tile of interest.
+        tile (str): Name of tile of interest.
         gridded_file (str): File of the gridded catalogue.
 
     Returns:
@@ -109,7 +106,7 @@ def flag_regions(cat_name: str, gridded_file: str) -> pd.DataFrame:
     """
     df = pd.read_csv(gridded_file)
 
-    mask = get_mask(cat_name)
+    mask = get_mask(tile)
     ny, nx = mask.shape
 
     x = df["X_IMAGE"].astype(int)
@@ -125,6 +122,44 @@ def flag_regions(cat_name: str, gridded_file: str) -> pd.DataFrame:
     
     print(f"Saved updated catalogue with mask info to: {gridded_file}")
     
+    return df
+
+def add_good_fraction(tile: str, centers_file: str):
+    """
+    Adds a 'good_fraction' column to centers_file based on the mask.
+
+    Parameters:
+        tile (str): Name of tile of interest.
+        centers_file (str): Path to the grid centers file. 
+            Must contain ['xmin', 'xmax', 'ymin', 'ymax'] columns.
+
+    Returns:
+        pd.DataFrame of the updated grid centres file.
+    """
+    mask = get_mask(tile)
+    ny, nx = mask.shape
+
+    df = pd.read_csv(centers_file)
+    good_fractions = []
+
+    for _, row in df.iterrows():
+        xmin = max(int(row["x_min"]), 0)
+        xmax = min(int(row["x_max"]), nx)
+        ymin = max(int(row["y_min"]), 0)
+        ymax = min(int(row["y_max"]), ny)
+
+        submask = mask[ymin:ymax, xmin:xmax]
+        total_pixels = submask.size
+        good_pixels = np.sum(submask)
+        fraction = good_pixels / total_pixels if total_pixels > 0 else np.nan
+
+        good_fractions.append(fraction)
+
+    df["good_fraction"] = good_fractions
+
+    df.to_csv(centers_file, index=False)
+
+    print(f"Added 'good_fraction' column and saved to: {centers_file}")
     return df
 
 def add_grid_counts(gridded_file: str, centers_file: str) -> None:
@@ -144,31 +179,27 @@ def add_grid_counts(gridded_file: str, centers_file: str) -> None:
     gridded_df = pd.read_csv(gridded_file)
     grid_info = pd.read_csv(centers_file)
 
-    required_columns = ["i", "j", "X_IMAGE", "Y_IMAGE", "GOOD_REGION"]
+    required_columns = ["i", "j", "X_IMAGE", "Y_IMAGE", 
+                        "GOOD_REGION", "good_fraction"]
     for col in required_columns:
         if col not in gridded_df.columns:
             raise ValueError(f"Input catalogue must have column '{col}'")
 
-    x = gridded_df["X_IMAGE"].astype(int)
-    y = gridded_df["Y_IMAGE"].astype(int)
+    good_counts = (
+        gridded_df[gridded_df["GOOD_REGION"]]
+        .groupby(["i", "j"])
+        .size()
+        .reset_index(name="good_count")
+        )
 
-    total_counts = gridded_df.groupby(["i", "j"]).size().reset_index(name="total_count")
-    good_counts = gridded_df[gridded_df["GOOD_REGION"]].groupby(["i", "j"]).size().reset_index(name="good_count")
-    merged_counts = pd.merge(total_counts, good_counts, on=["i", "j"], how="left")
-    
-    merged_counts["good_count"] = merged_counts["good_count"].fillna(0)
+    merged = pd.merge(grid_info, good_counts, on=["i", "j"], how="left")
 
-    merged_counts["good_fraction"] = merged_counts["good_count"] / merged_counts["total_count"]
-    merged_counts["good_fraction"] = merged_counts["good_fraction"].replace(0, np.nan)
-
-    merged_counts["normalized_count"] = merged_counts["good_count"] / merged_counts["good_fraction"]
-    merged_counts["normalized_count"] = merged_counts["normalized_count"].fillna(0).astype(int)
-
-    merged = pd.merge(grid_info, merged_counts[["i", "j", "normalized_count", "good_fraction"]],
-                      on=["i", "j"], how="left")
-
-    merged["normalized_count"] = merged["normalized_count"].fillna(0).astype(int)
-    merged["good_fraction"] = merged["good_fraction"].fillna(0)
+    merged["good_count"] = merged["good_count"].fillna(0)
+    merged["normalized_count"] = (
+        (merged["good_count"] / merged["good_fraction"])
+        .replace([np.inf, np.nan], 0)
+        .astype(int)
+        )
 
     merged.to_csv(centers_file, index=False)
     
